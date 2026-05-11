af5ce5d3b8cb0de06e0b6fe7c801cd183e8cd299
Miguel Taibo <dev@migueltaibo.com>
2026-05-11 23:01:17 +0200

    add home-manager

--- flake.nix
+++ flake.nix
@@ -4,18 +4,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # Grub theme repo
     grub2-themes.url = "github:vinceliuice/grub2-themes";
     grub2-themes.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, grub2-themes, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, grub2-themes, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         grub2-themes.nixosModules.default
+	home-manager.nixosModules.home-manager {
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.mtaibo = import ./home.nix;
+    	}
       ];
     };
   };
