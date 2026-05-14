41ecca950b2960b33928948001aea53b31b42a79
Miguel Taibo <dev@migueltaibo.com>
2026-05-14 12:33:46 +0200

    feat: add close alias for garage door via local API

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -11,6 +11,7 @@
       update = if pkgs.stdenv.hostPlatform.isDarwin
         then "sudo darwin-rebuild switch --flake ~/Dotfiles#macbook"
         else "sudo nixos-rebuild switch --flake ~/dotfiles#nixos";
+      close = "curl -X POST http://192.168.1.160/api/commands/B0301/down";
     };
 
     initContent = ''
