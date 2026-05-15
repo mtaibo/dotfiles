5392cb8d48f735284a7806c19cf8d0898cfbd488
Miguel Taibo <dev@migueltaibo.com>
2026-05-15 15:42:03 +0200

    remove: Raspberry Pi configuration (tphome)

--- flake.nix
+++ flake.nix
@@ -13,14 +13,9 @@
     };
     grub2-themes.url = "github:vinceliuice/grub2-themes";
     grub2-themes.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
   };
 
-  nixConfig = {
-    extra-substituters = [ "https://nixos-raspberrypi.cachix.org" ];
-    extra-trusted-public-keys = [ "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI=" ];
-  };
-  outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, grub2-themes, nixos-raspberrypi, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, grub2-themes, ... }: {
     homeConfigurations = {
       migueltaibo = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin;
@@ -43,26 +38,6 @@
             inherit inputs;
             pkgsUnstable = import inputs.nixpkgs-unstable {
               system = "x86_64-linux";
-              config.allowUnfree = true;
-            };
-          };
-        }
-      ];
-    };
-    nixosConfigurations.tphome = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      specialArgs = { inherit inputs nixos-raspberrypi; };
-      modules = [
-        ./hosts/rpi/default.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.migueltaibo = import ./hosts/rpi/home.nix;
-          home-manager.extraSpecialArgs = {
-            inherit inputs;
-            pkgsUnstable = import inputs.nixpkgs-unstable {
-              system = "aarch64-linux";
               config.allowUnfree = true;
             };
           };
