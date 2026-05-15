b898196203da8a5fd0644611c82e4f0c671f7e25
Miguel Taibo <dev@migueltaibo.com>
2026-05-15 08:06:00 +0200

    fix: add RPi5 armstub, U-Boot config, and correct config.txt to firmware module

--- modules/nixos/rpi/firmware.nix
+++ modules/nixos/rpi/firmware.nix
@@ -1,8 +1,27 @@
-{ ... }: {
+{ config, lib, pkgs, ... }: {
   boot.loader = {
     grub.enable = false;
     generic-extlinux-compatible.enable = true;
   };
 
   hardware.enableRedistributableFirmware = true;
+
+  # Write a correct config.txt and install RPi5 armstub on every rebuild.
+  # generic-extlinux-compatible handles U-Boot installation automatically.
+  system.activationScripts.rpi-boot = lib.mkAfter ''
+    cp -f ${pkgs.raspberrypifw}/share/raspberrypi/boot/armstub8-2712.bin /boot/armstub8-2712.bin
+
+    cat > /boot/config.txt << 'CONFIG'
+[all]
+arm_64bit=1
+enable_uart=1
+kernel=u-boot-rpi-arm64.bin
+armstub=armstub8-2712.bin
+dtoverlay=vc4-kms-v3d
+dtoverlay=
+[all]
+initramfs initrd followkernel
+os_check=0
+CONFIG
+  '';
 }
